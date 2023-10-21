6f5fa754b7546a72ab99a0879ca2745c36f65653
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-21 12:26:40 -0500

    Spring project initialized

---
+++ src/test/java/com/udea/gestionusuarios/GestionUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.gestionusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
