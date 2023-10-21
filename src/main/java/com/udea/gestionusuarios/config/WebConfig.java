714afc9df5861247c7be1585dcbf15e0f3f437cb
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-21 18:45:35 -0500

    Configuración del CORS y maven agregadas

---
+++ src/main/java/com/udea/gestionusuarios/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.udea.gestionusuarios.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
