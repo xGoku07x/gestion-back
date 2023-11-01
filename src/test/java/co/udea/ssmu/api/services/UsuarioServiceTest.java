349507132099fb2c0072738328f20ed60742285d
yendriferreira <yendri.ferreira@udea.edu.co>
2023-11-01 10:02:29 -0500

    code smells
    code smells reparado prueba

--- src/test/java/co/udea/ssmu/api/services/UsuarioServiceTest.java
+++ src/test/java/co/udea/ssmu/api/services/UsuarioServiceTest.java
@@ -4,18 +4,5 @@ import org.junit.jupiter.api.Test;
 
 public class UsuarioServiceTest {
 
-    @Test
-    void testActualizarUsuario() {
-    
-    }   
-    
-    @Test
-    void testCrearUsuario() {
-
-    }
-
-    @Test
-    void testObtenerInformacionUsuario() {
-
-    }
+  
 }
