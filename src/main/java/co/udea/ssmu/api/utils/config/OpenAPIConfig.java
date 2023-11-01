99772ca4aa623fc718823203891f3847609e64c5
yendriferreira <yendri.ferreira@udea.edu.co>
2023-11-01 11:40:48 -0500

    code smells
    code smells

--- src/main/java/co/udea/ssmu/api/utils/config/OpenAPIConfig.java
+++ src/main/java/co/udea/ssmu/api/utils/config/OpenAPIConfig.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
